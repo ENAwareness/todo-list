fba926fd47de0004a5b06969abde05e83be72ee1
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-02-23 11:05:56 +0900

    Add renderProjects() function and storage.js module
    - Implemented renderProjects() to display project list dynamically.
    - Created storage.js module for data persistence.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { createTodo } from "./todoFactory";
 import { createProject, addTodoToProject } from "./projectManager";
+import { saveToLocalStorage } from "./storage";
 
 let projects = [createProject("Default")];
 let currentProject = projects[0];
@@ -10,7 +11,25 @@ const addProjectForm = document.getElementById("add-project-form");
 const addTodoForm = document.getElementById("add-todo-form");
 const projectSelect = document.getElementById("new-todo-project");
 
-function renderProjects() {}
+function renderProjects() {
+    projectList.innerHTML = '';
+    projectSelect.innerHTML = '';
+    projects.forEach(project => {
+        const li = document.createElement('li');
+        li.textContent = project.name;
+        li.addEventListener('click', () => {
+            createProject = project;
+            renderTodos();
+        })
+        projectList.appendChild(li);
+
+        const option = document.createElement('option');
+        option.value = project.id;
+        option.textContent = project.name;
+        projectSelect.appendChild(option);
+    });
+    saveToLocalStorage('projects', projects);
+}
 
 function renderTodos() {}
 
